b653b1a9dab8f508102c68105eda0dd1c627b358
dwarad <dhanashreewaradk@gmail.com>
2026-05-23 02:45:45 +0530

    fix: copy index.css to build output directory

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    resources: [{ from: 'src/resources/', to: 'dist/resources/' }],
+    resources: [
+        { from: 'src/resources/', to: 'dist/resources/' },
+        { from: 'src/index.css', to: 'dist/index.css' }
+    ],
     webpackConfig: {
         resolve: {
             fallback: { querystring: require.resolve('querystring-es3') }
